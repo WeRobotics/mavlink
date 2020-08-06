fbb65de9b183d182ef02f3f683d35553e9ec049e
pajovic23 <vukpajovic95@gmail.com>
2020-08-06 16:00:24 +0200

    generate raspberry folder update

--- generate_project/generate_rasp.sh
+++ generate_project/generate_rasp.sh
@@ -1,10 +1,10 @@
 cd ./../pymavlink/generator/CPP11/test/posix
 make gen
 cd ./../../
-cp -r include_v2.0 /home/pi/WMP-ElevatorDonut
+cp -r include_v2.0 /home/pi/WMP-ElevatorDonut/communication/onboard_app_communication_cpp/TCP-onboard-commander
 cd ../javascript
 make 
 cd implementations/mavlink_release_mechanism_v2.0
 npm install
 cd ..
-cp -r mavlink_release_mechanism_v2.0 /home/pi/nodejs_onboard_router   
+cp -r mavlink_release_mechanism_v2.0 /home/pi/nodejs_onboard_router/node_modules   
