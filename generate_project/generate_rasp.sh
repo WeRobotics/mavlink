3c2b53aa9ff2c4e1f50efabaa3a627e7d120357a
pajovic23 <vukpajovic95@gmail.com>
2020-08-07 15:00:44 +0100

    generate_rasp script upgraded to have good paths

--- generate_project/generate_rasp.sh
+++ generate_project/generate_rasp.sh
@@ -1,7 +1,7 @@
 cd ./../pymavlink/generator/CPP11/test/posix
 make gen
 cd ./../../
-cp -r include_v2.0 /home/pi/WMP-ElevatorDonut/communication/onboard_app_communication_cpp/TCP-onboard-commander
+cp -r include_v2.0 /home/pi/WMP-ElevatorDonut/communication/onboard_app_communication_cpp/TCP-onboard-commander/mavlink
 cd ../javascript
 make 
 cd implementations/mavlink_release_mechanism_v2.0
