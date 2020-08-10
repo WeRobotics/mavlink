e66df5457ff0cba5bc17cd0985bf1d2e81e900d4
pajovic23 <vukpajovic95@gmail.com>
2020-08-10 11:50:43 +0200

    command added plus changed interface path

--- generate_project/generate_interface.sh
+++ generate_project/generate_interface.sh
@@ -1,3 +1,3 @@
 node ./../code_generator/index.js
-cp -r mavlink_enum.js /home/vuk/Documents/WeRobotics/Mosquito_code/WMP-OnBoard/interface/js/
+cp -r mavlink_enum.js /home/vuk/Documents/WeRobotics/Mosquito_code/Communication_git/communication/javascript_webinterface/interface/js
 rm mavlink_enum.js
